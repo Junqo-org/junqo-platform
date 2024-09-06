1e860b0527516e00321f117a06486bed619121c5
Viveli <maceo.jalbert@epitech.eu>
2024-09-06 15:50:19 +0900

    Update magidoc file and move schema file

---
+++ magidoc.mjs
@@ -0,0 +1,35 @@
+export default {
+    introspection: {
+        type: 'sdl',
+        paths: ['schemas/**/*.graphqls'],
+    },
+    website: {
+        template: 'carbon-multi-page',
+        output: 'docs/api',
+        staticAssets: 'docs/assets',
+        options: {
+            appTitle: 'Junqo Documentation',
+            appLogo: 'https://seeklogo.com/images/G/green-bird-logo-B0C45C520C-seeklogo.com.png',
+            siteRoot: '/docs/api',
+            siteMeta: {
+                description: 'The Junqo Project API Documentation',
+                keywords: 'docs,cool,42,api,junqo',
+            },
+            pages: [{
+                title: 'Introduction',
+                content: `
+                # Welcome to Junqo GraphQL API Documentation
+                `
+            }],
+            externalLinks: [
+                {
+                    href: 'https://github.com',
+                    label: 'Main repository',
+                    position: 'header',
+                    group: 'Repositories',
+                    kind: 'Github',
+                },
+            ],
+        },
+    },
+}
