badfaf3d4c49758ae245964d336699f2cc0b322a
florian <florian.azema@epitech.eu>
2024-10-27 17:00:03 +0800

    fix change

--- junqo_back/src/chat-gpt-api/chat-gpt-api.controller.spec.ts
+++ junqo_back/src/chat-gpt-api/chat-gpt-api.controller.spec.ts
@@ -1,10 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatgptController } from './chat-gpt-api.controller';
-import { ChatgptService } from './chat-gpt-api.service';
 
 describe('ChatgptController', () => {
   let chatgptController: ChatgptController;
-  let chatgptService: ChatgptService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
