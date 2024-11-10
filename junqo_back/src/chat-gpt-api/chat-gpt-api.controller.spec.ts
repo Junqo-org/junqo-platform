2efbdcd0bff678cffdeeea7c12ff40c12584244f
Vivelis <maceo.jalbert@gmail.com>
2024-11-10 18:32:42 +0900

    Fix missing service in back test

--- junqo_back/src/chat-gpt-api/chat-gpt-api.controller.spec.ts
+++ junqo_back/src/chat-gpt-api/chat-gpt-api.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatgptController } from './chat-gpt-api.controller';
+import { ChatgptService } from './chat-gpt-api.service';
 
 describe('ChatgptController', () => {
   let chatgptController: ChatgptController;
@@ -7,6 +8,7 @@ describe('ChatgptController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChatgptController],
+      providers: [ChatgptService],
     }).compile();
 
     chatgptController = module.get<ChatgptController>(ChatgptController);
