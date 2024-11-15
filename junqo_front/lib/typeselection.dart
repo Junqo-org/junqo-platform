f23c0ef0685853bff6ef2fab18b441fe27928eef
florian <florian.azema@epitech.eu>
2024-11-15 15:37:23 +0800

    front login and signup page

---
+++ junqo_front/lib/typeselection.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class Typeselection extends StatelessWidget {
+  const Typeselection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          margin: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _header(context),
+              _buttons(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header(context) {
+    return const Column(
+      children: [
+        Text(
+          "Comment junqo peut vous aider ?",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  _buttons(context) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+      const SizedBox(height: 32),
+      ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          backgroundColor: Color.fromARGB(255, 134, 159, 241),
+        ),
+        child: const Text(
+          "Trouvez un stage",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      const SizedBox(height: 32),
+      ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          backgroundColor: Color.fromARGB(255, 134, 159, 241),
+        ),
+        child: const Text(
+          "Trouvez un stage",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      const SizedBox(height: 32),
+      ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          backgroundColor: Color.fromARGB(255, 134, 159, 241),
+        ),
+        child: const Text(
+          "Trouvez un stage",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      const SizedBox(height: 32),
+      ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          backgroundColor: Color.fromARGB(255, 134, 159, 241),
+        ),
+        child: const Text(
+          "Trouvez un stage",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    ],
+  );
+}
+}
