524e7228c5e9830b4e3cc48b3b640b0099e9ff8a
florian <florian.azema@epitech.eu>
2024-12-01 17:29:56 +0800

    remove useless import

--- junqo_front/test/widget_test.dart
+++ junqo_front/test/widget_test.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:junqo_front/main.dart';
-import 'package:junqo_front/signup.dart';
 
 void main() {
   testWidgets('Test de navigation vers la page Signup', (WidgetTester tester) async {
