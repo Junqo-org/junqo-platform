48257e07059ca43e2e44553a7071fdd28e93d335
florian <florian.azema@epitech.eu>
2025-01-11 23:44:58 +0800

    readd missing file for client and graphql

--- junqo_front/lib/main.dart
+++ junqo_front/lib/main.dart
@@ -1,9 +1,22 @@
-import 'package:flutter/material.dart';
-import 'pages/welcome.dart';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:ferry/ferry.dart';
+import 'pages/welcome.dart';
+import 'package:junqo_front/client.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final client = await initClient();
+  // Register the client to be accessible globally
+  GetIt.instance.registerLazySingleton<Client>(() => client);
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
   @override
   Widget build (BuildContext context) {
     return const MaterialApp(
