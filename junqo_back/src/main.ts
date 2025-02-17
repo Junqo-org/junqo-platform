a37b46d09f7ac4d6badf9723853bc07ea12042d0
Vivelis <maceo.jalbert@gmail.com>
2025-02-18 02:16:02 +0900

    Remove debug

--- junqo_back/src/main.ts
+++ junqo_back/src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import 'dotenv/config';
-import { log } from 'console';
 
 const PORT_MIN = 1;
 const PORT_MAX = 65535;
@@ -42,7 +41,6 @@ async function bootstrap() {
       },
     }),
   );
-  log('CORS_ORIGINS ', CORS_ORIGINS);
   app.enableCors({
     origin: CORS_ORIGINS,
     methods: ['GET', 'POST', 'OPTIONS'],
