8f1ba7a710ce141b0b2d952deb214be7f862047f
Vivelis <maceo.jalbert@gmail.com>
2025-03-16 23:11:39 +0900

    Fix back offers dependencies

--- junqo_back/src/offers/offers.module.ts
+++ junqo_back/src/offers/offers.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OffersRepositoryModule } from './repository/offers.repository.module';
 import { OffersResolver } from './offers.resolver';
 import { OffersService } from './offers.service';
+import { CaslModule } from '../casl/casl.module';
 
 @Module({
-  imports: [OffersRepositoryModule],
+  imports: [OffersRepositoryModule, CaslModule],
   providers: [OffersResolver, OffersService],
 })
 export class OffersModule {}
