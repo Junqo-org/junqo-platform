a3953d95281ce19b5f883610599797624390afbf
Mickap1 <91679873+Mickap1@users.noreply.github.com>
2025-07-06 15:29:14 +0200

    11 front entreprise rercherche candidats2 (#135)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    * **New Features**
    * Introduced a recruiter-facing swipe card interface for browsing and
    recruiting student profiles.
    * Added a new "Recrutement" section to the company navigation bar,
    allowing access to the swipe card interface.
    * Added new API capabilities for fetching student profiles with filters
    and sending recruitment requests.
    * Updated routing to support navigation to the new recruiting interface.
    
    * **Content Updates**
    * Replaced placeholder messages with the full Privacy Policy and Terms
    of Use, both now scrollable and easily accessible.
    
    * **Improvements**
    * Enhanced formatting and consistency across various pages and API
    interactions for improved readability and user experience.
    * Updated company dashboard widget instantiation to support dynamic
    behavior.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Signed-off-by: Macéo JALBERT <42516894+Vivelis@users.noreply.github.com>
    Co-authored-by: Macéo JALBERT <42516894+Vivelis@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: macgameur <maceo.jalbert@gmail.com>

--- junqo_front/lib/router.dart
+++ junqo_front/lib/router.dart
@@ -19,6 +19,7 @@ import 'package:junqo_front/pages/welcome.dart';
 import 'package:junqo_front/shared/widgets/private_page.dart';
 import 'package:junqo_front/pages/offer_creation.dart';
 import 'package:junqo_front/pages/offer_list.dart';
+import 'package:junqo_front/pages/recruiter_recruiting.dart';
 import 'package:junqo_front/pages/interview_simulation_select.dart';
 
 class AppRouter {
@@ -73,6 +74,9 @@ class AppRouter {
         return MaterialPageRoute(
             builder: (_) => PrivatePage(
                 child: JobOfferForm(client: GetIt.instance<RestClient>())));
+      case '/swiping':
+        return MaterialPageRoute(
+            builder: (_) => PrivatePage(child: RecruiterRecruiting()));
       case '/register':
         final userType = settings.arguments;
 
