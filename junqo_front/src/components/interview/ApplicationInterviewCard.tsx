bb5fba89bcf799c5232d97210f785a593cbfb3ee
Vivelis <maceo.jalbert@gmail.com>
2026-02-01 00:44:26 +0100

    refactor: add React import to ApplicationInterviewCard.

--- junqo_front/src/components/interview/ApplicationInterviewCard.tsx
+++ junqo_front/src/components/interview/ApplicationInterviewCard.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
